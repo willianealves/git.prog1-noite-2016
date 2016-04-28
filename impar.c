42e70aced578a506603c3dff2f685bb53013bfa7
willianealves <williane-pinheiro@bol.com.br>
2016-04-28 01:05:33 +0000

    terceira aul

---
+++ impar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+{
+int main()
+
+
+    int i;
+    //numeros impares de 51 a 101
+    for(i=51; i<=101; i=i+2)
+    {
+        printf("\n%d\n",i);
+    }
+        
+      
+
+    
+    return 0;
+}
