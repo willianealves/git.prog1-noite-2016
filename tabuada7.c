e90a86159d50ac9cbdfb4d42d3594805b21f4195
willianealves <williane-pinheiro@bol.com.br>
2016-04-28 01:08:32 +0000

    tab

---
+++ tabuada7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+{
+   int i, multiplicar;
+   
+   printf("\nTabuada Multiplicacao do 7");
+   printf("\n__________________________");
+   
+   for(i=1;i<=10;i++)
+   {
+       
+       multiplicar=7*i;
+       printf("\n7x%d=%d",i,multiplicar);
+   }
+    
+
+
+
+
+    return 0;
+}
