fdd8abd33114ca1da0126223162bccd6d3b07a72
willianealves <williane-pinheiro@bol.com.br>
2016-04-28 00:25:17 +0000

    quarta aula

---
+++ contador.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+
+{
+    
+    int i;
+    for(i=1; i<=10; i++)
+    {
+        printf("\n%d\n",i);
+    }
+        
+        printf("\nUltimo valor de i=%d\n",i);
+
+
+
+    
+    return 0;
+}
+
